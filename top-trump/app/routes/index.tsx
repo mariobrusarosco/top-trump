29261b867754502d1fda20dac8d824be42b41009
Mario Brusarosco <mariobrusarosco@hotmail.com>
2022-11-05 20:58:29 -0300

    feat: add tailwind prettier support

--- top-trump/app/routes/index.tsx
+++ top-trump/app/routes/index.tsx
@@ -1,7 +1,7 @@
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1 className="text-3xl font-bold underline">Top Trump</h1>
+      <h1 className="bg-red text-3xl font-bold underline">Top Trump</h1>
       <ul>
         <li>
           <a
