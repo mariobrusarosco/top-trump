46a57642951f61dfa5c7f363f944c5c95ebbf5d0
Mario Brusarosco <mariobrusarosco@hotmail.com>
2022-11-21 08:01:41 -0300

    feat: socketio client setup

--- top-trump/app/routes/match-board.tsx
+++ top-trump/app/routes/match-board.tsx
@@ -1,7 +1,15 @@
 import Card from "~/components/Card"
 import Layout from "~/components/Layout/Layout"
+import { io } from 'socket.io-client'
+ 
+const URL = 'http://localhost:8000'
+const socket = io(URL, {
+    path: '/socket.io',
+    reconnection: false
+})
 
 const MatchBoard = () => {
+    console.log({ socket})
     return <section className="match-board">
         <Layout>
             <h1>Turn | Liz</h1>
