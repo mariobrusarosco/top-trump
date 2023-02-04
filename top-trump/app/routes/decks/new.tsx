32227c0043f9608c7eb797cc240bd2cca46bcf99
Mario Brusarosco <mariobrusarosco@hotmail.com>
2023-02-04 15:52:46 -0300

    (learning): Form - Server Side Validation

--- top-trump/app/routes/decks/new.tsx
+++ top-trump/app/routes/decks/new.tsx
@@ -1,10 +1,11 @@
 import type { ActionArgs } from "@remix-run/node";
 import { LoaderArgs, redirect } from "@remix-run/node";
 import { json } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+import { useActionData, useLoaderData } from "@remix-run/react";
 import Card from "~/components/Card";
 import Layout from "~/components/Layout/Layout";
 import { db } from "~/utils/db.server";
+import { badRequest } from "~/utils/request.server";
 
 export const action = async ({ request }: ActionArgs) => {
   const form = await request.formData();
@@ -13,11 +14,18 @@ export const action = async ({ request }: ActionArgs) => {
 
   const label = form.get("label") as string;
 
-  if (!label) {
-    throw new Error("label input field is null or invalid");
-  }
-
+  const fieldErrors = {
+    label: simpleFieldValidation(label),
+  };
   const fields = { label, image: "" };
+
+  if (Object.values(fieldErrors).some(Boolean)) {
+    return badRequest({
+      fieldErrors,
+      fields,
+      formError: null,
+    });
+  }
 
   const newDeck = await db.deck.create({
     data: fields,
@@ -27,7 +35,18 @@ export const action = async ({ request }: ActionArgs) => {
   return redirect(`/decks/${newDeck?.id}`);
 };
 
+function simpleFieldValidation(content: string) {
+  if (content?.length > 20) {
+    return "label is too long. Try less than 20 chars";
+  }
+
+  return null;
+}
+
 export default function NewDeck(props) {
+  const actionData = useActionData<typeof action>();
+  console.log({ actionData });
+
   return (
     <Layout>
       <h1 className="bg-red text-3xl font-bold  variant-test:text-teal-300">
@@ -37,8 +56,24 @@ export default function NewDeck(props) {
       <form method="post">
         <div>
           <label>
-            Name: <input type="text" name="label" />
+            Name:{" "}
+            <input
+              type="text"
+              name="label"
+              defaultValue={actionData?.fields?.label}
+              aria-invalid={
+                Boolean(actionData?.fieldErrors?.label) || undefined
+              }
+              aria-errormessage={
+                actionData?.fieldErrors?.label ? "label-error" : undefined
+              }
+            />
           </label>
+          {actionData?.fieldErrors?.label ? (
+            <p className="form-validation-error" role="alert" id="name-error">
+              {actionData.fieldErrors.label}
+            </p>
+          ) : null}
         </div>
 
         <div>
