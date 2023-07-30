9d23e0019cf0018415f800f85e132a00cb357101
Mario Brusarosco <mariobrusarosco@hotmail.com>
2023-07-30 19:19:26 -0300

    Project Checklist]: Jest - Add React Testing Library

---
+++ src/App.test.tsx
@@ -0,0 +1,11 @@
+import App from "./App";
+import { render } from "@testing-library/react";
+
+describe("Button", () => {
+  describe("when rendering", () => {
+    it("renders the text", () => {
+      render(<App />);
+      expect(false).toBe(true);
+    });
+  });
+});
