56230b4745b5cd7f2488157c3a1d2e9867a4ac2a
Mario Brusarosco <mariobrusarosco@hotmail.com>
2023-07-30 18:52:13 -0300

    [Project Checklist]: Jest - Transform TS files to JS to test

--- config/tests/jest/jest.config.ts
+++ config/tests/jest/jest.config.ts
@@ -1,7 +1,14 @@
-import type { Config } from "jest";
+import type { JestConfigWithTsJest } from "ts-jest";
 
-const config: Config = {
-  preset: "foo-bar",
+// To grab Jest Default Configuration
+// import { defaults } from "jest-config";
+
+// To grab the configuration that comes with 'ts-jest/presets/js-with-ts-esm',
+// import { jsWithTsESM } from "ts-jest/presets";
+
+const jestConfig: JestConfigWithTsJest = {
+  preset: "ts-jest/presets/js-with-ts-esm",
+  rootDir: "../../../src",
 };
 
-export default config;
+export default jestConfig;
