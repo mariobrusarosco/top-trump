fb7ce4be24953efbe97d0aeeba313cc3bc51deaa
Mario Brusarosco <mariobrusarosco@hotmail.com>
2023-08-28 18:45:06 -0300

    fix: removing folder generated by create-next-app command

--- src/domain/dashboard/screens/dashboard.tsx
+++
@@ -1,5 +0,0 @@
-export const DashboardScreen = () => {
-    return <section className="dashboard">
-        <h1>Dashboard</h1>
-    </section>
-}
--- src/domain/landing/index.tsx
+++
@@ -1,3 +0,0 @@
-export const LandingScreen = () => {
-    return <section><h2> Landing!</h2></section>
-}
