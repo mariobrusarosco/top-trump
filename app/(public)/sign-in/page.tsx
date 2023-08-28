d7bd20e7e0034ec0d1e99ffe705e9a48636e8823
Mario Brusarosco <mariobrusarosco@hotmail.com>
2023-08-28 20:19:01 -0300

    [LEARNING:next] - Create layout/organizational folders

---
+++ app/(public)/sign-in/page.tsx
@@ -0,0 +1,9 @@
+const SignInScreen = () => {
+  return (
+    <main>
+      <h2>Sign In</h2>
+    </main>
+  );
+};
+
+export default SignInScreen;
---
+++ app/(public)/sign-up/page.tsx
@@ -0,0 +1,9 @@
+const SignUpScreen = () => {
+  return (
+    <main>
+      <h2>Sign Up</h2>
+    </main>
+  );
+};
+
+export default SignUpScreen;
