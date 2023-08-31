ff231cc7e8c04dced61631ecbf76872b27208e44
Mario Brusarosco <mariobrusarosco@hotmail.com>
2023-08-31 19:58:26 -0300

    [LEARNING:prisma] - Find and Cretate Prisma Instances

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,13 +1,25 @@
 import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
+import { db } from "@/lib/db";
+import { initialProfile } from "@/lib/initial-profile";
 import { UserButton } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+  const profile = await initialProfile()
+  const server = await db.server.findFirst({
+    where: { members: { some: { profileId: profile.id }}}
+  })
+
+  if(server) redirect(`/servers/${server.id}`)
+
+  
   return (
     <div className="flex">
+
       <p className="text-3xl font-bold text--300">Top trump</p>
       <Button className="btn" variant="justAnExample">
-        asadsad
+        Create Server
       </Button>
       <UserButton afterSignOutUrl="/sign-in" />
       <ModeToggle />
