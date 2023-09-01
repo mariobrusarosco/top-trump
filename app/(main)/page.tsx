1d40aa3883d834fc01891cb22f4c44f70b30a0fd
Mario Brusarosco <mariobrusarosco@hotmail.com>
2023-08-31 21:51:49 -0300

    [LEARNING:shadcn/react-hooks-form/nextjs/zod] - Form with validation

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -4,6 +4,7 @@ import { db } from "@/lib/db";
 import { initialProfile } from "@/lib/initial-profile";
 import { UserButton } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
+import {InitialModal } from "@/components/modals/initial-modal"
 
 export default async function Home() {
   const profile = await initialProfile()
@@ -18,11 +19,12 @@ export default async function Home() {
     <div className="flex">
 
       <p className="text-3xl font-bold text--300">Top trump</p>
-      <Button className="btn" variant="justAnExample">
+      <Button className="btn">
         Create Server
       </Button>
       <UserButton afterSignOutUrl="/sign-in" />
       <ModeToggle />
+      <InitialModal />
     </div>
   );
 }
